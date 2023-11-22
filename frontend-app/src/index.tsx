73b9eac7e99906cc1a3230103dc66ad062ade733
Hillar Kapsta <kapstahillar@gmail.com>
2023-11-22 16:45:26 +0200

    Test pipeline

--- frontend-app/src/index.tsx
+++ frontend-app/src/index.tsx
@@ -18,4 +18,4 @@ root.render(
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-console.log("Server configured")
+console.log("Server configured asd")
